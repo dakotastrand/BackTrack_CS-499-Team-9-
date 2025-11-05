26e7549902c7a0c04cbd2c6144cb6569562f3144
Jamie <jami3507@gmail.com>
2025-11-05 10:22:55 -0600

    Reapply "Implement prototype timers"
    This reverts commit bf99f52449e4b30fbdfbb41c2d5941d8c42fc3c5.

--- App/hooks/useTimer.tsx
+++ App/hooks/useTimer.tsx
@@ -36,14 +36,20 @@ import useWebSocket from "./useWebSocket";
 
 export function useTimer() {
   const [timer, setTimer] = useState<number | null>(null);
+  // functions that get data from the websocket need to be declared before the websocket declaration
+  const completeTimer = () => {
+    setTimer(null);
+  }
+
   const socket = useWebSocket(
-    process.env.EXPO_PUBLIC_API_URL
+    process.env.EXPO_PUBLIC_API_URL, completeTimer
   )
 
+  // functions that send data via the websocket need to be declared after the websocket declaration
   const startTimer = (minutes: number) => {
     console.log("Starting timer for", minutes, "minutes");
     setTimer(minutes);
-    socket?.emit("data", "hello world")
+    socket?.emit("startTimer", minutes)
   };
 
   const startTimer = () => {
